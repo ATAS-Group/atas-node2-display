5292e0420a1743744af919a20e940985dfd4e921
schmm2 <tinsu3@gmail.com>
2017-12-15 21:08:21 +0100

    finish views

--- build/fonts/component_project_vars.mk
+++ build/fonts/component_project_vars.mk
@@ -1,6 +1,6 @@
 # Automatically generated build file. Do not edit.
 COMPONENT_INCLUDES += $(PROJECT_PATH)/components/Fonts/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/fonts -lFonts
+COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/fonts -lstdc++ -lFonts 
 COMPONENT_LINKER_DEPS += 
 COMPONENT_SUBMODULES += 
 COMPONENT_LIBRARIES += Fonts
--- main/main.cpp
+++ main/main.cpp
@@ -23,9 +23,9 @@
 //static const uint8_t SCK = 18; 	// yellow
 //static const uint8_t SS = 5; 		// orange
 
-static const uint8_t PIN_DC =  17; 	// green
-static const uint8_t PIN_RESET = 16; 	// white
-static const uint8_t PIN_BUSY = 4; 		// violett
+static const int PIN_DC =  17; 	// green
+static const int PIN_RESET = 16; 	// white
+static const int PIN_BUSY = 4; 		// violett
 
 GxIO_Class io(SPI, SS, PIN_DC, PIN_RESET); // arbitrary selection of 17, 16
 GxEPD_Class display(io, PIN_RESET, PIN_BUSY); // arbitrary selection of (16), 4
@@ -37,13 +37,53 @@ void initDisplay(){
 	display.init();
 }
 
-void displayAlarmOn(){
+void displayAlarmOn(){	
 	display.setFont(fontsans12);
-	display.setTextColor(GxEPD_BLACK);
 	display.setCursor(48, 175);
 	display.fillScreen(GxEPD_WHITE);
 	display.println("Alarm ON");
 	display.drawBitmap(alarmbell, 36, 16, 128, 128, GxEPD_BLACK);
+	display.update();
+}
+
+void displayDashboard(){
+	display.setFont(fontsans9);	
+	display.fillScreen(GxEPD_WHITE);
+	display.drawBitmap(location, 10, 10, 40, 40, GxEPD_BLACK);
+	display.setCursor(60, 20);
+	display.println("Lat: 46.212134");
+	display.setCursor(60, 45);
+	display.println("Lng: 7.8932157");
+	display.update();
+}
+
+void displayAlarm(int alarm){
+	display.setFont(fontsans12);
+	display.setCursor(48, 175);
+	display.fillScreen(GxEPD_WHITE);
+	
+	switch(alarm) {
+	    case 1 : 
+			display.drawBitmap(avalanche, 36, 16, 128, 128, GxEPD_BLACK);
+			display.println("Avalanche");
+			printf("Triggered Alarm: Avalanche\n");
+			break;
+	    case 2 : 
+			display.drawBitmap(landslide, 36, 16, 128, 128, GxEPD_BLACK);
+			display.println("Landslide");
+			printf("Triggered Alarm: Landslide\n");
+	    	break;
+		case 3:
+			display.drawBitmap(snowflake, 36, 16, 128, 128, GxEPD_BLACK);
+			display.setCursor(42, 175);
+			display.println("Snowstorm");
+			printf("Triggered Alarm: Snowstorm\n");
+			break;
+		default:
+			display.println("Error");
+			printf("displayAlarm: error, no alarm defined");
+			break;
+	}
 	display.update();
 }
 
@@ -55,25 +95,13 @@ extern "C" void app_main()
 	printf("ATAS Node 2 - Display Example!\n");
 	
 	initDisplay();
-	displayAlarmOn();
-		
-	// IMAGE
-	//display.drawBitmap(img, x , y, width, height, color);
-	display.drawBitmap(avalanche, 36, 36, 128, 128, GxEPD_BLACK);
-	
-    // FONT Setup
 	display.setTextColor(GxEPD_BLACK);
-	display.setFont(fontsans9);
-		
-	// GPS Data
-	display.setCursor(10, 190);
-	display.println("Lng: 45.662101");
-	display.setCursor(10, 165);
-	display.println("Lat: 7.234189");
-		
-	display.update();
 	
-
+	displayDashboard();
+	displayAlarmOn();
+	displayAlarm(1);
+	displayAlarm(2);
+	displayAlarm(3);
 }
 
 
